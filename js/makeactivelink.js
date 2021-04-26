db665383759730b365bb6ed329d110c616f78fe3
unknown <sriker.ambati@gmail.com>
2021-04-26 22:54:18 +0100

    initial work

--- js/makeactivelink.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function ($) {
-    var path = window.location.pathname;
-    console.log("The path is ", path);
-    $('#menu li a[href = "' + path + '" ]').parent().addClass("current");
-});
